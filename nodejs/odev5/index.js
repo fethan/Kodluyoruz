a5df171aaa8edeaa6d56ce5ad31ce0d9b86eb1c6
fethan <fethan.us@gmail.com>
2021-10-27 18:09:36 +0300

    odev5 completed

---
+++ nodejs/odev5/index.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+
+
+    if (url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+        res.write("<h2>Index sayfasına hoşgeldiniz</h2>");
+    } else if (url === "/hakkimda") {
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+        res.write("<h2>Hakkımda sayfasına hoşgeldiniz</h2>");
+    } else if (url === "/iletisim") {
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+        res.write("<h2>İletişim sayfasına hoşgeldiniz</h2>");
+    }
+
+    res.end();
+});
+
+const port = 5000;
+
+server.listen(port, () => {
+    console.log(`Sunucu port ${port} de başlatıldı.`);
+});
