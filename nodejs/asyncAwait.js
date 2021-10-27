bb5d380af94aa7e523f7fa0a7d5a48a0eb33bba7
fethan <fethan.us@gmail.com>
2021-10-27 12:19:08 +0300

    async await

--- nodejs/asyncAwait.js
+++ nodejs/asyncAwait.js
@@ -1,11 +1,52 @@
-const func = async () => {
-    let promise = new Promise((resolve, reject) => {
-        setTimeout(() => { resolve("Tamamlandı!") }, 2000);
-    });
+/*
+    const func = async () => {
+        let promise = new Promise((resolve, reject) => {
+            setTimeout(() => { resolve("Tamamlandı!") }, 2000);
+        });
 
-    let result = await promise;
+        let result = await promise;
 
-    console.log(result);
-}
+        console.log(result);
+    }
 
 func();
+*/
+
+function getData(data) {
+    return new Promise((resolve, reject) => {
+        console.log("Veriler alınıyor.");
+
+        if (data) {
+            resolve("Veriler alındı.");
+        }
+        else {
+            reject("Veriler alınamadı.");
+        }
+    });
+};
+
+function cleanData(receivedData) {
+    return new Promise((resolve, reject) => {
+        console.log("Veriler düzenleniyor.");
+
+        if (receivedData) {
+            resolve("Veriler düzenlendi.");
+        }
+        else {
+            reject("Veriler düzenlenemedi.");
+        }
+    })
+}
+
+async function processData() {
+    try {
+        const receivedData = await getData(true);
+        console.log(receivedData);
+        const cleanedData = await cleanData(false);
+        console.log(cleanedData);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+processData();
